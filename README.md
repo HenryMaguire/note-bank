95e88f75b69334cbfd48f0a12d58b5261ef67fa2
Henry <henrymaguire@hotmail.co.uk>
2017-01-23 21:11:51 +0000

    Users can select working dir. Big update to README.

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # Overview
 This is a small command line-based productivity app, based on the pomodoro technique. You start out by writing down what you aim to achieve over the next 5 sessions. Then a timer is set and and you get to work. When the timer finishes a box pops up which prompts you to write a brief summary of what you did in the last session. The entry is saved in a text file along with the time and date. You then have a short break. Rinse and repeat.
 
-# Structure
-- Basic timer will is a sleep function, followed by a pop-up/sound.
-- A rest timer begins - another sleep timer followed by something (maybe an intrusive pop-up/sound).
+# System requirements
+As of yet, this will only work in OSX because it relies on osascript. I'm working on making it compatible with all platforms!
 
+# Getting started
+1. Open up `note-bank.bash` in your favourite text editor.
+2. Change the variable `DIR` to the working directory you would like to save your notes to
+3. In `file_path` change `notes.txt` to whatever you want your notes file to be called.
+4. Run the app in terminal by entering `bash note-bank.bash` (make sure you're in the application directory).
+5. Follow all of the
 
+# New features
+- Users can input their own working directory, so the notes are saved elsewhere on the computer (rather than just next to the .bash file). This means users can easily work on different threads/projects by just changing the file_path variable. It also keeps my personal notes separate from the app so can access it freely.
 
 # Future features
 - Different projects could have different threads
--- note-bank.bash
+++ note-bank.bash
@@ -1,25 +1,28 @@
 # Program for Pomodoro timer
 #!/bin/bash
-echo "`date +%Y-%m-%d`" >> notes.txt
+DIR="/Users/henrymaguire/Work/phd-work/note-bank"
+mkdir -p $DIR
+file_path="$DIR/notes.txt"
+echo "`date +%Y-%m-%d`" >> $file_path
 echo -e "What would you like to achieve in these next five sessions?"
 read begin_note
 echo -e "Summarise your mood in one word:"
 read mood_note
-echo "`date +%H:%M:%S`" >> notes.txt
-echo "Mood: $mood_note" >> notes.txt
-echo "$begin_note" >> notes.txt
+echo "`date +%H:%M:%S`" >> $file_path
+echo "Mood: $mood_note" >> $file_path
+echo "$begin_note" >> $file_path
 for a in 1 2 3 4
 do
   echo -e "It's `date +%H:%M:%S`. Get to work!"
   osascript -e 'display notification "Work for 18 minutes." with title "Go!" sound name "Submarine"'
   sleep 1080
   #sleep 480
-  echo -e "What did you get up to in Pomodoro $a?"
+  echo -e "What did you get up to in Pomodoro $a? Include a plan of the next Pomodoro."
   osascript -e 'display notification "You have 2 minutes to summarise in terminal." with title "Stop!" sound name "Purr"'
   read note
-  echo "`date +%H:%M:%S`" >> notes.txt
-  echo "$note" >> notes.txt
-  echo >> notes.txt
+  echo "`date +%H:%M:%S`" >> $file_path
+  echo "$note" >> $file_path
+  echo >> $file_path
   echo "Good job! Now rest for 5 minutes."
   osascript -e 'display notification "Walk around for 5 minutes." with title "Rest!" sound name "Hero"'
   sleep 300
@@ -33,8 +36,8 @@ sleep 1080
 osascript -e 'display notification "Summarise last session in terminal." with title "Stop!" sound name "Purr"'
 echo -e "What did you get up to in Pomodoro 5?"
 read final_note
-echo "`date +%H:%M:%S`" >> notes.txt
-echo "$final_note" >> notes.txt
-echo >> notes.txt
+echo "`date +%H:%M:%S`" >> $file_path
+echo "$final_note" >> $file_path
+echo >> $file_path
 osascript -e 'display notification "Take a long break." with title "Rest!" sound name "Ping"'
 echo "Good job! Now take a long rest."
