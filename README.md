db756875b1434126459fb45c5f9ad33725039b12
Henry <henrymaguire@hotmail.co.uk>
2017-01-26 13:56:33 +0000

    Refactor and improve functionality
    Users can now choose how long rests, study times and summary times are.
    When you start a session it gives an estimate of how long this takes.
    Time-stamp formatting within notes improved.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This is a small command line-based productivity app, based on the pomodoro technique. You start out by writing down what you aim to achieve over the next 5 sessions. Then a timer is set and and you get to work. When the timer finishes a box pops up which prompts you to write a brief summary of what you did in the last session. The entry is saved in a text file along with the time and date. You then have a short break. Rinse and repeat.
 
 # System requirements
-As of yet, this will only work in OSX because it relies on osascript. I'm working on making it compatible with all platforms!
+As of yet, this will only work in OSX because it relies on *osascript*. I'm working on making it compatible with all platforms!
 
 # Getting started
 1. Open up `note-bank.bash` in your favourite text editor.
@@ -12,7 +12,7 @@ As of yet, this will only work in OSX because it relies on osascript. I'm workin
 5. Follow all of the directions in terminal
 
 # New features
-- Users can input their own working directory, so the notes are saved elsewhere on the computer (rather than just next to the .bash file). This means users can easily work on different threads/projects by just changing the file_path variable. It also keeps my personal notes separate from the app so can access it freely.
+- Users can input their own working directory, so the notes are saved elsewhere on the computer (rather than just next to the .bash file). This means users can easily work on different threads/projects by just changing the file_path variable. It also keeps my personal notes separate from the app so other users can access it freely without them finding out my secrets ;)
 
 # Future features
 - Compatibility with linux and Windows. Perhaps would need to rewrite everything in Python and use some of the available resources.
--- note-bank.bash
+++ note-bank.bash
@@ -3,43 +3,45 @@
 DIR="/Users/henrymaguire/Work/phd-work/note-bank"
 mkdir -p $DIR
 file_path="$DIR/notes.txt"
-echo "`date +%Y-%m-%d`" >> $file_path
-echo "`date +%H:%M:%S`" >> $file_path
+pom_number=5; study_length=20; rest_length=5; summary_length=5
+pom_length=$((study_length+rest_length+summary_length))
+session_length=$((pom_number*pom_length))
+#Here we will make sure the user is ready for this session. Log a time and date stamp.
+echo -e "\nPress return when you are ready to start your set of $pom_number pomodoros.\nThis will take roughly $((session_length/60)) hours and $((session_length%60)) minutes."
+read ready
+echo "`date +%Y-%m-%d`" >> $file_path; echo "`date +%H:%M:%S`" >> $file_path
 echo -e "What would you like to achieve in these next five sessions?"
 read begin_note
 echo -e "Summarise your mood in one word:"
 read mood_note
 echo "Mood: $mood_note" >> $file_path
 echo "$begin_note" >> $file_path
-for a in 1 2 3 4
+END=$((pom_number-1)) # Final pomodoro ends with a different message so we keep that one out of the loop
+for a in $(seq 1 $END)
 do
-  echo -e "It's `date +%H:%M:%S`. Get to work!"
+  echo -e "It's `date +%H:%M:%S`. Get to work! You have $study_length minutes."
   echo "`date +%H:%M:%S`" >> $file_path
-  osascript -e 'display notification "Work for 18 minutes." with title "Go!" sound name "Submarine"'
-  sleep 1080
-  #sleep 480
+  osascript -e 'display notification "Get to work." with title "Go!" sound name "Submarine"'
+  sleep $((study_length*60))
   echo -e "What did you get up to in Pomodoro $a? Include a plan of the next Pomodoro."
-  osascript -e 'display notification "You have 2 minutes to summarise in terminal." with title "Stop!" sound name "Purr"'
+  osascript -e 'display notification "Go and summarise in terminal." with title "Stop!" sound name "Purr"'
   read note
-  echo "`date +%H:%M:%S`" >> $file_path
-  echo "$note" >> $file_path
-  echo >> $file_path
-  echo "Good job! Now rest for 5 minutes."
-  osascript -e 'display notification "Walk around for 5 minutes." with title "Rest!" sound name "Hero"'
-  sleep 300
+  echo -e "$note \n`date +%H:%M:%S`" >> $file_path
+  echo "Good job! Now rest for $rest_length minutes."
+  osascript -e 'display notification "Walk around for a few minutes." with title "Rest!" sound name "Hero"'
+  sleep $((rest_length*60))
   echo "Your break is over, press enter to begin the next session:"
   osascript -e 'display notification "Begin next session in terminal." with title "Break is over!" sound name "Purr"'
   read fluff
 done
 echo -e "It's `date +%H:%M:%S`. Get to work!"
 echo "`date +%H:%M:%S`" >> $file_path
-osascript -e 'display notification "Work for 18 minutes." with title "Go!" sound name "Submarine"'
-sleep 1080
+osascript -e 'display notification "Get to work." with title "Go!" sound name "Submarine"'
+sleep $((study_length*60))
 osascript -e 'display notification "Summarise last session in terminal." with title "Stop!" sound name "Purr"'
 echo -e "What did you get up to in Pomodoro 5?"
 read final_note
-echo "`date +%H:%M:%S`" >> $file_path
 echo "$final_note" >> $file_path
-echo >> $file_path
+echo "`date +%H:%M:%S`" >> $file_path
 osascript -e 'display notification "Take a longer break." with title "Rest!" sound name "Ping"'
 echo "Good job! Now take a longer rest. Come back in about 20 minutes."
